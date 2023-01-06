032a43a26e50237769514d2eaa7e7f97e5128e66
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-01-06 16:13:25 +0530

    Create Requirements.sh

---
+++ Requirements.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Requirements
+sudo apt-get -y update;
+sudo apt-get -y upgrade;
+sudo apt-get install -y golang;
+sudo apt-get install -y subfinder;
+sudo apt-get install -y amass;
+sudo apt-get -y update;
+sudo apt-get -y upgrade;
+
+echo "Requirements installation finished";
+
+#Satya Prakash
+#Security Researcher and Bug Hunter
+#Follow me on linkedin.com/in/0xKayala
+#Follow me on twitter.com/0xKayala
+#About me on about.me/satyakayala
