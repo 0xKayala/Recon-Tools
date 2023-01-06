4ffb6216164feac736c8c15d327e491bf036e90e
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-01-06 19:18:08 +0530

    Update Requirements.sh

--- Requirements.sh
+++ Requirements.sh
@@ -3,9 +3,19 @@
 # Install Requirements
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
-sudo apt-get install -y golang;
-sudo apt-get install -y subfinder;
 sudo apt-get install -y amass;
+sudo apt-get install -y golang;
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
+mv go/bin/nuclei /usr/bin/
+mv go/bin/httpx /usr/bin/
+mv go/bin/subfinder /usr/bin/
+mv go/bin/naabu /usr/bin/
+mv go/bin/httpx /usr/bin/
+mv go/bin/interactsh-client /usr/bin/
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
 
