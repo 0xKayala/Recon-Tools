d2482ba687daed839bef3d3a2e012e9016c4095d
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-01-06 19:27:33 +0530

    Update Requirements.sh

--- Requirements.sh
+++ Requirements.sh
@@ -4,18 +4,19 @@
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
 sudo apt-get install -y amass;
+sudo apt install nmap
+sudo apt install sublist3r
 sudo apt-get install -y golang;
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-mv go/bin/nuclei /usr/bin/
-mv go/bin/httpx /usr/bin/
-mv go/bin/subfinder /usr/bin/
-mv go/bin/naabu /usr/bin/
-mv go/bin/httpx /usr/bin/
-mv go/bin/interactsh-client /usr/bin/
+sudo mv go/bin/katana /usr/bin/
+sudo mv go/bin/httpx /usr/bin/
+sudo mv go/bin/subfinder /usr/bin/
+sudo mv go/bin/naabu /usr/bin/
+sudo mv go/bin/interactsh-client /usr/bin/
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
 
