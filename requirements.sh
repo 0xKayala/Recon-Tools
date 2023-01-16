1549b44f0a7bd94bd4fba66869c7b7e53f3ee5f0
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-01-16 23:52:43 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -13,29 +13,17 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-sudo mv go/bin/nuclei /usr/bin/
-sudo mv /root/go/bin/nuclei /usr/bin/
 sudo mv /home/kali/go/bin/nuclei /usr/bin/
-sudo mv go/bin/katana /usr/bin/
-sudo mv /root/go/bin/katana /usr/bin/
 sudo mv /home/kali/go/bin/katana /usr/bin/
-sudo mv go/bin/httpx /usr/bin/
-sudo mv /root/go/bin/httpx /usr/bin/
 sudo mv /home/kali/go/bin/httpx /usr/bin/
-sudo mv go/bin/subfinder /usr/bin/
-sudo mv /root/go/bin/subfinder /usr/bin/
 sudo mv /home/kali/go/bin/subfinder /usr/bin/
-sudo mv go/bin/naabu /usr/bin/
-sudo mv /root/go/bin/naabu /usr/bin/
 sudo mv /home/kali/go/bin/naabu /usr/bin/
-sudo mv go/bin/interactsh-client /usr/bin/
-sudo mv /root/go/bin/interactsh-client /usr/bin/
 sudo mv /home/kali/go/bin/interactsh-client /usr/bin/
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
 
 echo "Requirements installation finished";
 
-Satya Prakash
-Security Researcher and Bug Hunter
-About me on about.me/satyakayala
+echo "Satya Prakash";
+echo "Security Researcher and Bug Hunter";
+echo "About me on about.me/satyakayala";
